2d08db00751fa797bf9d2509a60b7761a8149650
ArShuranov <arshuranov@yandex.ru>
2023-09-29 21:05:28 +0700

    test commit

--- src/main/java/com/example/telegramanimalshelterholiday/TelegramAnimalShelterHolidayApplication.java
+++ src/main/java/com/example/telegramanimalshelterholiday/TelegramAnimalShelterHolidayApplication.java
@@ -10,6 +10,8 @@ public class TelegramAnimalShelterHolidayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TelegramAnimalShelterHolidayApplication.class, args);
+		System.out.println("Hello world!");
 	}
+
 
 }
