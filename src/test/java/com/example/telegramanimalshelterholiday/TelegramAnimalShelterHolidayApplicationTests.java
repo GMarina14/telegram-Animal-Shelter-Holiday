5eeda9fa50de51e3e933b77002a68a2256843e3f
BuenosDiasGente <morkovka03121981@gmail.com>
2023-09-29 11:54:42 +0300

    Creating an empty project in SpringBoot

---
+++ src/test/java/com/example/telegramanimalshelterholiday/TelegramAnimalShelterHolidayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telegramanimalshelterholiday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramAnimalShelterHolidayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
