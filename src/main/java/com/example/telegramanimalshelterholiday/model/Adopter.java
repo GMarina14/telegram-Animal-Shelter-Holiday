3d12bba252283ccbdd41a5ed6a37242eabc20410
ArShuranov <arshuranov@yandex.ru>
2023-10-02 23:04:26 +0700

    Все заработало, можно в develop

--- src/main/java/com/example/telegramanimalshelterholiday/model/Adopter.java
+++ src/main/java/com/example/telegramanimalshelterholiday/model/Adopter.java
@@ -1,36 +1,108 @@
 package com.example.telegramanimalshelterholiday.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-import java.util.List;
+import java.util.Objects;
 
 @Entity
-@Data
-public class Adopter extends User{
+public class Adopter extends Client {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
-    private Contract contract;
+    //private Contract contract;
     private Integer probExtend;
 
+    public Adopter(Long chatId, String userName, String phoneNumber, Long id, String firstName, String lastName, Integer probExtend) {
+        super(chatId, userName, phoneNumber);
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.probExtend = probExtend;
+    }
+
+    public Adopter(Long id, String firstName, String lastName, Integer probExtend) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.probExtend = probExtend;
+    }
+
+    public Adopter(Long chatId, String userName, String phoneNumber) {
+        super(chatId, userName, phoneNumber);
+    }
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getProbExtend() {
+        return probExtend;
+    }
+
+    public void setProbExtend(Integer probExtend) {
+        this.probExtend = probExtend;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Adopter adopter = (Adopter) o;
+        return Objects.equals(id, adopter.id) && Objects.equals(firstName, adopter.firstName) && Objects.equals(lastName, adopter.lastName) && Objects.equals(probExtend, adopter.probExtend);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), id, firstName, lastName, probExtend);
+    }
+
+    @Override
+    public String toString() {
+        return "Adopter{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", probExtend=" + probExtend +
+                '}';
+    }
+}
 
 
-    @JsonIgnore
+    /* @JsonIgnore
     @OneToMany(mappedBy = "adopter")
     private List<Animal> animals;
 
-    /*на строне Animal нам нужно добавить следующее
+    *//*на строне Animal нам нужно добавить следующее
     @ManyToOne
     @JoinColumn(name = "adopter_id")
-    private Adopter adopter;*/
+    private Adopter adopter;*//*
 
 
     @JsonIgnore
     @OneToMany(mappedBy = "adopter")
-    private List<Report> reports;
+    private List<Report> reports;*/
 
-}
+
--- src/main/java/com/example/telegramanimalshelterholiday/model/Report.java
+++ src/main/java/com/example/telegramanimalshelterholiday/model/Report.java
@@ -1,19 +1,19 @@
 package com.example.telegramanimalshelterholiday.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import lombok.Data;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 import java.time.LocalDate;
-import java.util.List;
+import java.util.Arrays;
+import java.util.Objects;
 
 @Entity
-@Data
 public class Report {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private LocalDate reportDate;
     private byte[] photo;
@@ -21,13 +21,96 @@ public class Report {
     private String stateOfHealth;
     private String behavior;
 
+    public Report(LocalDate reportDate, byte[] photo, String diet, String stateOfHealth, String behavior) {
+        this.reportDate = reportDate;
+        this.photo = photo;
+        this.diet = diet;
+        this.stateOfHealth = stateOfHealth;
+        this.behavior = behavior;
+    }
+
+    public Report() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public LocalDate getReportDate() {
+        return reportDate;
+    }
+
+    public void setReportDate(LocalDate reportDate) {
+        this.reportDate = reportDate;
+    }
+
+    public byte[] getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(byte[] photo) {
+        this.photo = photo;
+    }
+
+    public String getDiet() {
+        return diet;
+    }
+
+    public void setDiet(String diet) {
+        this.diet = diet;
+    }
+
+    public String getStateOfHealth() {
+        return stateOfHealth;
+    }
+
+    public void setStateOfHealth(String stateOfHealth) {
+        this.stateOfHealth = stateOfHealth;
+    }
+
+    public String getBehavior() {
+        return behavior;
+    }
+
+    public void setBehavior(String behavior) {
+        this.behavior = behavior;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Report report = (Report) o;
+        return Objects.equals(id, report.id) && Objects.equals(reportDate, report.reportDate) && Arrays.equals(photo, report.photo) && Objects.equals(diet, report.diet) && Objects.equals(stateOfHealth, report.stateOfHealth) && Objects.equals(behavior, report.behavior);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(id, reportDate, diet, stateOfHealth, behavior);
+        result = 31 * result + Arrays.hashCode(photo);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Report{" +
+                "id=" + id +
+                ", reportDate=" + reportDate +
+                ", photo=" + Arrays.toString(photo) +
+                ", diet='" + diet + '\'' +
+                ", stateOfHealth='" + stateOfHealth + '\'' +
+                ", behavior='" + behavior + '\'' +
+                '}';
+    }
+
+    /*
     @ManyToOne
     @JoinColumn(name = "adopter_id")
     private Adopter adopter;
 
     @ManyToOne
     @JoinColumn(name = "animal_id")
-    private Animal animal;
+    private Animal animal;*/
 
     /*на стороне Animal
     @JsonIgnore
