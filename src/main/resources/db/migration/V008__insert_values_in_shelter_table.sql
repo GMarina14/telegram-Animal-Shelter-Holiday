d499e2e16b5c1a6637d2352b66bcf2fbeea38210
BuenosDiasGente <morkovka03121981@gmail.com>
2023-10-10 13:58:59 +0300

    В базу данных добавлено 2 приюта.

--- src/main/resources/db/migration/V008__insert_values_in_shelter_table.sql
+++ src/main/resources/db/migration/V008__insert_values_in_shelter_table.sql
@@ -1,2 +1,3 @@
 INSERT INTO shelter (id,shelter_name,pets_species,city,address,phone_number,path_description)
-VALUES (1,'Shelter cat','CAT','Moscow','Lenina 24','+79109108997','https://yandex.ru/maps/-/CDaUBLJc');
+VALUES (1,'Shelter cat','CAT','Moscow','Lenina 24','+79109108997','https://yandex.ru/maps/-/CDaUBLJc'),
+       (2,'Shelter dog','DOG','Moscow','Brysilova 14','+79807654567','https://yandex.ru/maps/-/CDaUvQ7O');
