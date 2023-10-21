96378f905b32833d9f082a7847a4f8eb85caa31c
BuenosDiasGente <morkovka03121981@gmail.com>
2023-10-21 21:45:53 +0300

    new

--- src/main/java/com/example/telegramanimalshelterholiday/component/HandlerVolunteer.java
+++ src/main/java/com/example/telegramanimalshelterholiday/component/HandlerVolunteer.java
@@ -6,55 +6,42 @@ import com.example.telegramanimalshelterholiday.service.MessageService;
 import com.example.telegramanimalshelterholiday.service.VolunteerService;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Update;
-import com.pengrad.telegrambot.request.SendMessage;
-import com.pengrad.telegrambot.response.SendResponse;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import static com.example.telegramanimalshelterholiday.constants.MenuButtonsConst.*;
-import static java.util.Objects.isNull;
+import static com.example.telegramanimalshelterholiday.constants.MenuButtonsConst.CONTACT_USER;
+import static com.example.telegramanimalshelterholiday.constants.MenuButtonsConst.NO_VOLUNTEERS;
 
 @Component
 @RequiredArgsConstructor
 public class HandlerVolunteer {
     private static final Logger logger = LoggerFactory.getLogger(HandlerVolunteer.class);
-    private final TelegramBot telegramBot;
     private final VolunteerService volunteerService;
     private final MessageService messageService;
 
     /**
-     *
      * @param update
      * @param chatIdC
      */
     public void callVolunteer(Update update, long chatIdC) {
-        //String userId = ""; // client's chat_id or username
-        // userId = userId + update.message().from().id();
         long userId = chatIdC; // user's chat_id
         logger.info("UserId = {}", userId);
         Volunteer randomVolunteer;
 
         try {
-          randomVolunteer = volunteerService.getRandomVolunteer();
-        }catch (NotFoundConfigException e){
+            randomVolunteer = volunteerService.getRandomVolunteer();
+        } catch (NotFoundConfigException e) {
             logger.error("Volunteer not found");
             System.out.println(e.getMessage());
             System.out.println("Exception was processed. Program continues");
             messageService.sendMessage(userId, NO_VOLUNTEERS);
             return;
         }
-            // Volunteer chat_id. Send message to volunteer.
-            messageService.sendMessage(randomVolunteer.getChatId(), String.format(CONTACT_USER, userId));
+        // Volunteer chat_id. Send message to volunteer.
+        messageService.sendMessage(randomVolunteer.getChatId(), String.format(CONTACT_USER, userId));
 
-    }
-
-    private void sendMessage(SendMessage message) {
-        SendResponse response = telegramBot.execute(message);
-        if (!isNull(response) && !response.isOk()) {
-            logger.warn("Message was not sent: {}, error code: {}", message, response.errorCode());
-        }
     }
 
 }
--- src/main/java/com/example/telegramanimalshelterholiday/repository/AdopterRepository.java
+++ src/main/java/com/example/telegramanimalshelterholiday/repository/AdopterRepository.java
@@ -3,7 +3,6 @@ package com.example.telegramanimalshelterholiday.repository;
 import com.example.telegramanimalshelterholiday.model.Adopter;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
@@ -16,6 +15,11 @@ public interface AdopterRepository extends JpaRepository<Adopter, Long> {
      */
     @Query(value = "SELECT * FROM adopter", nativeQuery = true)
     Collection<Adopter> getAll();
+
+    /**
+     * Получения Адоптера по chatId
+     */
+    Adopter findByChatId(long chatId);
 
     //Эта штука пока не работает, это связано видимо с тем, что phoneNumber в таблице клиент, а таблицы Adopter у меня вообще нет
     /*@Query(value = "SELECT * FROM adopter WHERE phone_number = :phoneNumber", nativeQuery = true)
